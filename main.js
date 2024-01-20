cd42bbe8be26b08249c7cdc81d83adadd309b57e
robsonbsouza <souza.sp@hotmail.com>
2024-01-20 16:18:21 -0300

    V1

---
+++ main.js
@@ -0,0 +1,10 @@
+alert('******BEM VINDO AO SHOW DO MILHÃO******');
+
+var iniciarJogo = confirm('Deseja iniciar o jogo? \n\n Digite 1 para sim \n\n Digite 2 para não');
+
+if (iniciarJogo == true) {
+    alert('jogo iniciado')
+}else {
+    alert('Jogo encerrado')
+};
+
