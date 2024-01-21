5572dac659788296b1054be71d727f54e2235615
unknown <felipe_3c1@hotmail.com>
2024-01-21 17:13:23 -0300

    V2.1

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-alert('******BEM VINDO AO SHOW DO MILHÃO******');
+pergunta1 = prompt('Vamos a pergunta que vale 50,000 Mil Reais  \n\n Quantas patas tem um cachorro?\n\n1-Seis \n 2-Três \n 3-quatro \n 4-uma');
 
-var iniciarJogo = confirm('Clique em OK para iniciar o jogo ou CANCELAR para encerrar.');
+pergunta2 = prompt('Vamos a pergunta que vale 80,000 Mil Reais \n\n Qual é o Fenômenos atmosféricos em que a lua cobre o sol?\n\n1-Big bag \n2-Meteoro \n3-Tsunami \n4- Eclipse');
 
-if (iniciarJogo == false) {
-    alert('Jogo encerrado');
-};
+pergunta3 = prompt('Vamos a pergunta que vale 100,000 Mil Reais \n\n Quantas horas tem o dia?\n\n1-Nove \n2-Vinte e Quatro \n3-Dezessete \n4-Doze');
 
-var resposta = prompt('Pergunta numero 1 \n\n Qual a cor do carro do Robson? \n\n 1-Azul \n 2-Vermelho \n 3-Prata \n 4-Verde ');
-var correta = 3;
+pergunta4 = prompt('Vamos a pergunta que vale 1500,000 Mil Reais \n\n Na igreja católica, o responsável pela missa é o?\n\n1-Pastor \n2-Buda \n3-Padre \n4-Rabino');
 
-if (resposta == 3){
-    alert('parabens');
-    }else {
-        alert('errado');
-    };
+pergunta5 = prompt('Vamos a pergunta que vale 400,000 Mil Reais \n\n Quantas estações tem um ano?\n\n1-Quatro\n2-Oito \n3-Duas \n4-Cinco');
 
+pergunta6 = prompt('Vamos a pergunta que vale 600,000 Mil Reais \n\n Qual o nome do parceiro do batman?\n\n1-Branca de neve \n2-Sherek \n3-Robbin \n4-Pinoquio');
 
+prgunta7 = prompt('Vamos a pergunta que vale 700,000 Mil Reais \n\n  Em que mês se comemora o natal?\n\n1-Março \n2-Desembro \n3-Junho \n4-Abril');
+
+pergunta8 = prompt('Vamos a pergunta que vale 800,000 Mil Reais \n\n Que tipo de animal é o morcego?\n\n1-Comelão \n2-Mamifero \n3-Aracnídeos \n4-Herbivoros');
+
+perguta9 = prompt('Vamos a pergunta que vale 900,000 Mil Reais \n\n O tomate é?\n\n1-Um Doce \n2-Fruta \n3-Tuberculo \n4-Verdura');
+
+pergunta10 =prompt('***PARABENS*** \n\n Você chegou a pergunta que vale 1000,000 Milhão De Reais\n\n Qual personagem bíblico abriu o mar vermelho\n\n1-Tobias \n2-Daniel \n3-Moises \n4-Jose'); 
 
 
